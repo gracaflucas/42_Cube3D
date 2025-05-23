34b461653d810d1a9faada79ed315407706b40eb
Lucas <lufiguei@c2r2s6.42porto.com>
2025-05-23 16:51:54 +0100

    altered old movement to work as expected

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ana-lda- <ana-lda-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lufiguei <lufiguei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 12:12:02 by ana-lda-          #+#    #+#             */
-/*   Updated: 2025/05/23 13:43:37 by ana-lda-         ###   ########.fr       */
+/*   Updated: 2025/05/23 16:50:58 by lufiguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,69 @@
 // The AWSD keys are suppoused to move the player, the up|down|left|right keys are suppoused to turn left and right thru the map
 #include "cub3D.h"
 
-/* static int	is_valid_move(t_data *game, double x, double y)
+static int	is_valid_move(t_data *game, double new_x, double new_y)
 {
-	int	mx;
-	int	my;
+	int	map_x = (int)new_x;
+	int	map_y = (int)new_y;
+	int	cur_x = (int)game->px;
+	int	cur_y = (int)game->py;
 
-	mx = (int)x;
-	my = (int)y;
-	if (game->map[my][mx] == '1')
-		return (0);
-	game->px = x;
-	game->py = y;
+	if (game->map_array[cur_y][map_x] != '1')
+		game->px = new_x;
+	if (game->map_array[map_y][cur_x] != '1')
+		game->py = new_y;
 	return (1);
-} */
+}
 
 static int	key_hook(int keysym, t_data *game)
 {
+	double	new_px;
+	double	new_py;
 	if (keysym == XK_Escape)
 		close_window(game);
 	if (keysym == XK_Left)
 		game->player_angle -= ROT_SPEED;
 	if (keysym == XK_Right)
 		game->player_angle += ROT_SPEED;
-	if (keysym == KEY_W || keysym == KEY_S || keysym == KEY_A || keysym == KEY_D)
-		move_player(keysym, game);
+	if (keysym == KEY_W)
+	{
+		new_px = game->px + cos(game->player_angle) * MOVE_SPEED;
+		new_py = game->py + sin(game->player_angle) * MOVE_SPEED;
+		is_valid_move(game, new_px, new_py);
+	}
+	if (keysym == KEY_S)
+	{
+		new_px = game->px - cos(game->player_angle) * MOVE_SPEED;
+		new_py = game->py - sin(game->player_angle) * MOVE_SPEED;
+		is_valid_move(game, new_px, new_py);
+	}
+	if (keysym == KEY_D)
+	{
+		new_px = game->px + cos(game->player_angle + PI / 2) * MOVE_SPEED;
+		new_py = game->py + sin(game->player_angle + PI / 2) * MOVE_SPEED;
+		is_valid_move(game, new_px, new_py);
+	}
+	if (keysym == KEY_A)
+	{
+		new_px = game->px + cos(game->player_angle - PI / 2) * MOVE_SPEED;
+		new_py = game->py + sin(game->player_angle - PI / 2) * MOVE_SPEED;
+		is_valid_move(game, new_px, new_py);
+	}
 	return (render_map(game), 0);
 }
+
+// static int	key_hook(int keysym, t_data *game)
+// {
+// 	if (keysym == XK_Escape)
+// 		close_window(game);
+// 	if (keysym == XK_Left)
+// 		game->player_angle -= ROT_SPEED;
+// 	if (keysym == XK_Right)
+// 		game->player_angle += ROT_SPEED;
+// 	if (keysym == KEY_W || keysym == KEY_S || keysym == KEY_A || keysym == KEY_D)
+// 		move_player(keysym, game);
+// 	return (render_map(game), 0);
+// }
 
 int	main(int argc, char **argv)
 {
